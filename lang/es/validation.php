17dbb8f3eaac963981e4158a33e056b5792d7b43
FidelEliseoReyesRamirez <fidelrey000@gmail.com>
2026-04-30 01:05:58 -0400

    Idioma en español

---
+++ lang/es/validation.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'required' => 'El campo :attribute es obligatorio.',
+    'string' => 'El campo :attribute debe ser texto.',
+    'email' => 'El campo :attribute debe ser un correo electrónico válido.',
+    'max' => 'El campo :attribute no debe ser mayor que :max caracteres.',
+    'min' => 'El campo :attribute debe tener al menos :min caracteres.',
+    'confirmed' => 'La confirmación de :attribute no coincide.',
+    'unique' => 'El valor de :attribute ya está registrado.',
+    'same' => 'El campo :attribute debe coincidir con :other.',
+    'boolean' => 'El campo :attribute debe ser verdadero o falso.',
+
+    'attributes' => [
+        'name' => 'nombre',
+        'email' => 'correo electrónico',
+        'password' => 'contraseña',
+        'password_confirmation' => 'confirmación de contraseña',
+        'remember' => 'recordarme',
+    ],
+];
